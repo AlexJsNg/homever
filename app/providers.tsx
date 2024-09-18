034e73d1ea7d41d9074cdfd0199d07e1bbba48e1
Oleksandr Drakin <oleksandr.drakin@valtech.com>
2024-09-18 19:14:21 +0300

    airbnb: add sign(in/up/out) links

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,10 @@
 'use client';
+import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "./theme-provider";
 
 function Providers({ children }: { children: React.ReactNode }) {
     return <>
+        <Toaster></Toaster>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
             {children}
         </ThemeProvider>
