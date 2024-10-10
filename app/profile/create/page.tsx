df8fbea5de696be5045b6a155d8d4e79e7546304
Oleksandr Drakin <oleksandr.drakin@valtech.com>
2024-10-10 09:41:16 +0300

    add basic profile saving

---
+++ app/profile/create/page.tsx
@@ -0,0 +1,31 @@
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import FormInput from '@/components/form/FormInput';
+import FormContainer from '@/components/form/FormContainer';
+import { SubmitButton } from '@/components/form/Button';
+const createProfileAction = async (prevState: any, formData: FormData) => {
+  'use server';
+  const firstName = formData.get('firstName') as string;
+  console.log(firstName);
+  return { message: 'Profile created' };
+};
+
+function CreateProfilePage() {
+  return (
+    <section>
+      <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
+      <div className='border p-8 rounded-md max-w-lg'>
+        <FormContainer action={createProfileAction}>
+          <div className='grid gap-4 mt-4'>
+            <FormInput type='text' name='firstName' label='First Name' />
+            <FormInput type='text' name='lastName' label='Last Name' />
+            <FormInput type='text' name='username' label='Username' />
+          </div>
+          <SubmitButton text='Create Profile' className='mt-8' />
+        </FormContainer>
+      </div >
+    </section >
+  );
+}
+export default CreateProfilePage;
