34c6487e0572f575b546e3398917f44fa440d49d
AlexJsNg <alex.js.ng@gmail.com>
2024-12-27 20:24:18 +0200

    working on home page: prepare categories and properties list; initial data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,23 @@
-function HomePage() {
-  return <h1 className='text-3xl'>HomePage</h1>;
+import CategoriesList from '@/components/home/CategoriesList';
+import PropertiesContainer from '@/components/home/PropertiesContainer';
+
+function HomePage({
+  searchParams,
+}: {
+  searchParams: { category?: string; search?: string };
+}) {
+  
+  return (
+    <section>
+      <CategoriesList
+        category={searchParams?.category}
+        search={searchParams?.search}
+      />
+      <PropertiesContainer
+        category={searchParams?.category}
+        search={searchParams?.search}
+      />
+    </section>
+  );
 }
 export default HomePage;
