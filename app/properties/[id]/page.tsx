e8e04d455cd6ae1934e795c5c43e1a09e3ca7319
AlexJsNg <alex.js.ng@gmail.com>
2025-01-23 15:27:15 +0200

    add calendar holder

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,4 +1,6 @@
 import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
+import PropertyRating from '@/components/card/PropertyRating';
+import BookingCalendar from '@/components/properties/booking/BookingCalendar';
 import BreadCrumbs from '@/components/properties/BreadCrumbs';
 import ImageContainer from '@/components/properties/ImageContainer';
 import ShareButton from '@/components/properties/ShareButton';
@@ -17,11 +19,22 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
       <header className='flex justify-between items-center mt-4'>
         <h1 className='text-4xl font-bold '>{property.tagline}</h1>
         <div className='flex items-center gap-x-4'>
-        <ShareButton name={property.name} propertyId={property.id} />
-        <FavoriteToggleButton propertyId={property.id} />
+          <ShareButton name={property.name} propertyId={property.id} />
+          <FavoriteToggleButton propertyId={property.id} />
         </div>
       </header>
       <ImageContainer mainImage={property.image} name={property.name} />
+      <section className='lg:grid lg:grid-cols-12 gap-x-12 mt-12'>
+        <div className='lg:col-span-8'>
+          <div className='flex gap-x-4 items-center'>
+            <h1 className='text-xl font-bold'>{property.name}</h1>
+            <PropertyRating inPage propertyId={property.id} />
+          </div>
+        </div>
+        <div className='lg:col-span-4 flex flex-col items-center'>
+          <BookingCalendar></BookingCalendar>
+        </div>
+      </section>
     </section>
   );;
 }
