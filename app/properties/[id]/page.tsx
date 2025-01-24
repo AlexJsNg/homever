4e52ff791c517e6fd32e8eb6e7936f172b23822b
AlexJsNg <alex.js.ng@gmail.com>
2025-01-24 22:09:10 +0200

    extend property page: amenities, description

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,11 +1,14 @@
 import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
 import PropertyRating from '@/components/card/PropertyRating';
+import Amenities from '@/components/properties/Amenities';
 import BookingCalendar from '@/components/properties/booking/BookingCalendar';
 import BreadCrumbs from '@/components/properties/BreadCrumbs';
+import Description from '@/components/properties/Description';
 import ImageContainer from '@/components/properties/ImageContainer';
 import PropertyDetails from '@/components/properties/PropertyDetails';
 import ShareButton from '@/components/properties/ShareButton';
 import UserInfo from '@/components/properties/UserInfo';
+import { Separator } from '@/components/ui/separator';
 import { fetchPropertyDetails } from '@/utils/actions';
 import { redirect } from 'next/navigation';
 
@@ -36,6 +39,9 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
           </div>
           <PropertyDetails details={details}></PropertyDetails>
           <UserInfo profile={{ firstName, profileImage }} />
+          <Separator className='mt-4' />
+          <Description description={property.description} />
+          <Amenities amenities={property.amenities} />
         </div>
         <div className='lg:col-span-4 flex flex-col items-center'>
           <BookingCalendar></BookingCalendar>
