b77a999b03c90649598bcf326526ccaad5ccf0f5
AlexJsNg <alex.js.ng@gmail.com>
2026-06-29 17:45:30 +0300

    fix hostname for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'img.clerk.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'zaxzinifnhnhjhwrjuzs.supabase.co',
-        },
-      ],
-    },
-  };
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'blzgsepbizbweefbjuwv.supabase.co',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
